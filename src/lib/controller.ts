e60c7730f33460f32e041b04042b14340e5b9c73
Daryl Collins <daryl@darylcollins.com>
2023-05-15 19:11:56 +0100

    Initial cut of FastAuthController class with biometric bindings, key caching and delegate action signing using near-js packages

--- src/lib/controller.ts
+++ src/lib/controller.ts
@@ -1,17 +1,120 @@
+import { KeyPairEd25519 } from '@near-js/crypto';
+import { InMemoryKeyStore } from '@near-js/keystores';
+import { Account, Connection } from '@near-js/accounts';
+import { createKey, getKeys } from '@near-js/biometric-ed25519';
+import { baseEncode } from 'borsh';
+import { networkParams } from './networkParams';
+
 class FastAuthController {
-    private privateKey: Window | null;
-    private publicKey: Window | null;
+    private accountId: string;
+    private networkId: string;
+    private keyStore: InMemoryKeyStore;
+    private connection: Connection;
 
-    constructor() { }
+    constructor({ accountId, networkId }) {
+        const config = networkParams[networkId];
+        if (!config) {
+            throw new Error(`Invalid networkId ${networkId}`);
+        }
 
-    isSignedIn() {
-        return !!this.privateKey;
+        this.connection = Connection.fromConfig({
+            networkId,
+            provider: { type: 'JsonRpcProvider', args: { url: config.nodeUrl, headers: config.headers } },
+            signer: { type: 'InMemorySigner', keyStore: this.keyStore },
+        });
+
+        this.networkId = networkId;
+        this.accountId = accountId;
+
+        this.keyStore = new InMemoryKeyStore();
     }
 
-    getPublicKey() { }
-    getAccounts() { }
-    signTransaction() { }
-    signDelegateAction() { }
+    async createBiometricKey() {
+        const keyPair = await createKey(this.accountId);
+        await this.setKey(keyPair);
+
+        return keyPair;
+    }
+
+    async getCorrectAccessKey(accountId, firstKeyPair, secondKeyPair) {
+        const account = new Account(this.connection, this.accountId);
+
+        const accessKeys = await account.getAccessKeys();
+
+        const firstPublicKeyB58 = "ed25519:" + baseEncode((firstKeyPair.getPublicKey().data));
+        const secondPublicKeyB58 = "ed25519:" + baseEncode((secondKeyPair.getPublicKey().data));
+
+        const accessKey = accessKeys.find((accessKey) => accessKey.public_key === firstPublicKeyB58 || secondPublicKeyB58);
+        if (!accessKey) {
+            throw new Error('No access key found');
+        } else if (accessKey.public_key === firstPublicKeyB58) {
+            return firstKeyPair;
+        } else {
+            return secondKeyPair;
+        }
+    }
+
+    private async getBiometricKey() {
+        const [firstKeyPair, secondKeyPair] = await getKeys(this.accountId);
+        const privKeyStr = await this.getCorrectAccessKey(this.accountId, firstKeyPair, secondKeyPair);
+
+        return new KeyPairEd25519(privKeyStr.split(':')[1]);
+    }
+
+
+    async getKey() {
+        return this.keyStore.getKey(this.networkId, this.accountId);
+    }
+
+    async setKey(keyPair) {
+        return this.keyStore.setKey(this.networkId, this.accountId, keyPair);
+    }
+
+    async isSignedIn() {
+        return !!(await this.getKey());
+    }
+
+    assertValidSigner(signerId) {
+        if (signerId && signerId !== this.accountId) {
+            throw new Error(`Cannot sign transactions for ${signerId} while signed in as ${this.accountId}`);
+        }
+    }
+
+    async getPublicKey() {
+        let keyPair = await this.getKey();
+
+        if (!keyPair) {
+            const biometricKeyPair = await this.getBiometricKey();
+            await this.setKey(biometricKeyPair);
+
+            keyPair = biometricKeyPair;
+        }
+
+        return keyPair.getPublicKey().toString();
+    }
+
+    async getAccounts() {
+        if (this.accountId) {
+            return [this.accountId];
+        }
+
+        return [];
+    }
+
+    async signDelegateAction({ receiverId, actions, signerId }) {
+        this.assertValidSigner(signerId);
+
+        const account = new Account(this.connection, this.accountId);
+        return await account.signedDelegate({
+            actions,
+            blockHeightTtl: 60,
+            receiverId,
+        });
+    }
+
+    async signTransaction(params) {
+        return this.signDelegateAction(params);
+    }
 }
 
-export default new FastAuthController();
+export default FastAuthController;
---
+++ src/lib/networkParams.ts
@@ -0,0 +1,21 @@
+export const networkParams = {
+    mainnet: {
+        networkId: 'mainnet',
+        nodeUrl: 'https://rpc.mainnet.near.org',
+        walletUrl: 'https://wallet.near.org',
+        helperUrl: 'https://helper.mainnet.near.org',
+    },
+    testnet: {
+        networkId: 'testnet',
+        nodeUrl: 'https://rpc.testnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org',
+        helperUrl: 'https://helper.testnet.near.org',
+    },
+    localnet: {
+        // these are defined by https://github.com/kurtosis-tech/near-package
+        networkId: 'localnet',
+        nodeUrl: 'http://127.0.0.1:8332',
+        walletUrl: 'http://127.0.0.1:8334',
+        helperUrl: 'http://127.0.0.1:8330',
+    },
+};
