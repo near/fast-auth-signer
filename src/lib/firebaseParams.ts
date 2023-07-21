48cfc09020ecb95a3fd2751ff5f370a58710ef9b
Danny Cho <hj.danny.cho@gmail.com>
2023-07-21 17:10:00 +1200

    chore: setup firebase config

---
+++ src/lib/firebaseParams.ts
@@ -0,0 +1,29 @@
+export default {
+  mainnet: {
+    apiKey:            'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
+    authDomain:        'near-fastauth-prod.firebaseapp.com',
+    projectId:         'near-fastauth-prod',
+    storageBucket:     'near-fastauth-prod.appspot.com',
+    messagingSenderId: '829449955812',
+    appId:             '1:829449955812:web:532436aa35572be60abff1',
+    measurementId:     'G-T2PPJ8QRYY',
+  },
+  testnet: {
+    apiKey:            'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
+    authDomain:        'pagoda-oboarding-dev.firebaseapp.com',
+    projectId:         'pagoda-oboarding-dev',
+    storageBucket:     'pagoda-oboarding-dev.appspot.com',
+    messagingSenderId: '116526963563',
+    appId:             '1:116526963563:web:053cb0c425bf514007ca2e',
+    measurementId:     'G-HF2NBGE60S',
+  },
+  localnet: {
+    apiKey:            'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
+    authDomain:        'pagoda-oboarding-dev.firebaseapp.com',
+    projectId:         'pagoda-oboarding-dev',
+    storageBucket:     'pagoda-oboarding-dev.appspot.com',
+    messagingSenderId: '116526963563',
+    appId:             '1:116526963563:web:053cb0c425bf514007ca2e',
+    measurementId:     'G-HF2NBGE60S',
+  },
+};
