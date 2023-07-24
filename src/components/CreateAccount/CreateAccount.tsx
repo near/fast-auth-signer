0099c1fb02a5401164319530d94d29b4e4ad12e7
amirsaran3 <amir.saran@sqa-consulting.com>
2023-07-24 11:18:09 +0200

    Add biometric

--- src/components/CreateAccount/CreateAccount.tsx
+++ src/components/CreateAccount/CreateAccount.tsx
@@ -1,3 +1,4 @@
+import { createKey } from '@near-js/biometric-ed25519';
 import * as React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import './CreateAccount.css';
@@ -5,10 +6,20 @@ import './CreateAccount.css';
 function CreateAccount() {
   const [searchParams] = useSearchParams();
 
+  const [email, setEmail] = React.useState('');
+  const [username, setUsername] = React.useState('');
+
   React.useEffect(() => {
     console.log(searchParams.get('publicKey'));
     console.log(searchParams.get('redirectUrl'));
-  });
+  }, []);
+
+  async function onSubmitHandler(e) {
+    e.preventDefault();
+    console.log('asdasdasd', email, username);
+    const key = await createKey(username);
+    console.log(key);
+  }
 
   return (
     <div className="modal">
@@ -16,14 +27,14 @@ function CreateAccount() {
         <h1>Create Account</h1>
         <p>Use this account to sign in everywhere on NEAR, no password required.</p>
       </div>
-      <form>
+      <form onSubmit={onSubmitHandler}>
         <div>
           <p className="label">Email</p>
-          <input type="email" name="email" id="email" placeholder="user_name@email.com" />
+          <input type="email" name="email" id="email" placeholder="user_name@email.com" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div>
           <p className="label">Account ID</p>
-          <input type="text" name="account-id" id="account-id" placeholder="user_name.near" />
+          <input type="text" name="account-id" id="account-id" placeholder="user_name.near" value={username} onChange={(e) => setUsername(e.target.value)} />
           <p className="label label-bottom">Use a suggested ID or customize your own.</p>
         </div>
         <button type="submit">Continue</button>
