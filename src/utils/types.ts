6acc264e9969ca91730dd3efad91c788e4b6e8ac
Haris Valjevcic <haris.valjevcic@sqa-consulting.com>
2023-08-11 10:41:34 +0200

    Merge main into branch

---
+++ src/utils/types.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+import type { ReactElement, ReactNode } from 'react';
+
+export type NetworkId = ProductionNetwork['networkId'];
+export type Network = ProductionNetwork;
+// export type NetworkId = ProductionNetwork['networkId'] | DevelopmentNetwork['networkId'];
+// export type Network = ProductionNetwork | DevelopmentNetwork;
+
+type ProductionNetwork = {
+  networkId: 'testnet' | 'mainnet';
+  viewAccountId: string;
+  nodeUrl: string;
+  walletUrl: string;
+  helperUrl: string;
+  fastAuth: {
+    mpcRecoveryUrl: string;
+    authHelperUrl: string; // TODO refactor: review by fastauth team
+    accountIdSuffix: string;
+    firebase: {
+      apiKey: string;
+      authDomain: string;
+      projectId: string;
+      storageBucket: string;
+      messagingSenderId: string;
+      appId: string;
+      measurementId: string;
+    };
+  };
+};
+
+// type DevelopmentNetwork = {
+//   networkId: 'localnet';
+//   viewAccountId: string;
+//   nodeUrl: string;
+//   walletUrl: string;
+//   helperUrl: string;
+// };
