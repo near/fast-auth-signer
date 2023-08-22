b9eb604ba7f888d14000831b32c56cb7159d1768
esaminu <esaminu1@icloud.com>
2023-08-22 07:55:51 +0400

    fix: font paths

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,13 @@ module.exports = {
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use:  [{
+          loader:  'file-loader',
+          options: {
+            name:       '[name].[ext]',
+            outputPath: './fonts/'
+          }
+        }],
         type: 'asset/resource',
       },
     ],
