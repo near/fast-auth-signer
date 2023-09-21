a379168f5d24dddb126cc8adb10fedf491f913d6
esaminu <esaminu1@icloud.com>
2023-09-22 02:04:17 +0400

    chore: config via webpack

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,20 +7,20 @@ export const networks: Record<NetworkId, Network> = {
     nodeUrl:       'https://rpc.mainnet.near.org',
     walletUrl:     'https://wallet.near.org',
     helperUrl:     'https://helper.mainnet.near.org',
-    relayerUrl:    process.env.RELAYER_URL || 'https://near-relayer-mainnet.api.pagoda.co/relay',
+    relayerUrl:    process.env.RELAYER_URL,
     explorerUrl:   'https://explorer.near.org',
     fastAuth:      {
       mpcRecoveryUrl:  'https://mpc-recovery-leader-mainnet-cg7nolnlpa-ue.a.run.app',
       authHelperUrl:   'https://api.kitwallet.app',
       accountIdSuffix: 'near',
       firebase:        {
-        apiKey:            process.env.FIREBASE_API_KEY || 'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
-        authDomain:        process.env.FIREBASE_AUTH_DOMAIN || 'near-fastauth-prod.firebaseapp.com',
-        projectId:         process.env.FIREBASE_PROJECT_ID || 'near-fastauth-prod',
-        storageBucket:     process.env.FIREBASE_STORAGE_BUCKET || 'near-fastauth-prod.appspot.com',
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '829449955812',
-        appId:             process.env.FIREBASE_APP_ID || '1:829449955812:web:532436aa35572be60abff1',
-        measurementId:     process.env.FIREBASE_MEASUREMENT_ID || 'G-T2PPJ8QRYY',
+        apiKey:            process.env.FIREBASE_API_KEY,
+        authDomain:        process.env.FIREBASE_AUTH_DOMAIN,
+        projectId:         process.env.FIREBASE_PROJECT_ID,
+        storageBucket:     process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId:             process.env.FIREBASE_APP_ID,
+        measurementId:     process.env.FIREBASE_MEASUREMENT_ID,
       },
     },
   },
@@ -30,20 +30,20 @@ export const networks: Record<NetworkId, Network> = {
     nodeUrl:       'https://rpc.testnet.near.org',
     walletUrl:     'https://wallet.testnet.near.org',
     helperUrl:     'https://helper.testnet.near.org',
-    relayerUrl:    process.env.RELAYER_URL_TESTNET || 'http://34.70.226.83:3030/relay',
+    relayerUrl:    process.env.RELAYER_URL_TESTNET,
     explorerUrl:    'https://explorer.testnet.near.org',
     fastAuth:      {
       mpcRecoveryUrl:  'https://mpc-recovery-leader-testnet-cg7nolnlpa-ue.a.run.app',
       authHelperUrl:   'https://testnet-api.kitwallet.app',
       accountIdSuffix: 'testnet',
       firebase:        {
-        apiKey:            process.env.FIREBASE_API_KEY_TESTNET || 'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
-        authDomain:        process.env.FIREBASE_AUTH_DOMAIN_TESTNET || 'pagoda-oboarding-dev.firebaseapp.com',
-        projectId:         process.env.FIREBASE_PROJECT_ID_TESTNET || 'pagoda-oboarding-dev',
-        storageBucket:     process.env.FIREBASE_STORAGE_BUCKET_TESTNET || 'pagoda-oboarding-dev.appspot.com',
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_TESTNET || '116526963563',
-        appId:             process.env.FIREBASE_APP_ID_TESTNET || '1:116526963563:web:053cb0c425bf514007ca2e',
-        measurementId:     process.env.FIREBASE_MEASUREMENT_ID_TESTNET || 'G-HF2NBGE60S',
+        apiKey:            process.env.FIREBASE_API_KEY_TESTNET,
+        authDomain:        process.env.FIREBASE_AUTH_DOMAIN_TESTNET,
+        projectId:         process.env.FIREBASE_PROJECT_ID_TESTNET,
+        storageBucket:     process.env.FIREBASE_STORAGE_BUCKET_TESTNET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID_TESTNET,
+        appId:             process.env.FIREBASE_APP_ID_TESTNET,
+        measurementId:     process.env.FIREBASE_MEASUREMENT_ID_TESTNET,
       },
     },
   },
@@ -57,6 +57,6 @@ export const networks: Record<NetworkId, Network> = {
   // },
 };
 
-export const networkId: NetworkId = (process.env.NETWORK_ID as NetworkId) || 'mainnet';
+export const networkId: NetworkId = (process.env.NETWORK_ID as NetworkId);
 export const network = networks[networkId];
-export const basePath = process.env.REACT_APP_BASE_PATH || '';
+export const basePath = process.env.REACT_APP_BASE_PATH;
