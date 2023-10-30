e7fa9d4390d5440a887137cbdea0d3f970a2d2d6
Danny Cho <hj.danny.cho@gmail.com>
2023-10-30 13:28:28 +1300

    feat: initial sentry setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import i18next from 'i18next';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
@@ -6,6 +7,7 @@ import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import lang_de from './translations/de.json';
 import lang_en from './translations/en.json';
+import { networkId } from './utils/config';
 
 i18next.init({
   interpolation: { escapeValue: false }, // React already does escaping
@@ -18,6 +20,29 @@ i18next.init({
       common: lang_de
     },
   },
+});
+
+Sentry.init({
+  environment:           networkId,
+  dsn:          'https://bc7ad0bdc2de28704b4b695e2c3f3e35@o4506119713390592.ingest.sentry.io/4506119715028992',
+  integrations: [
+    new Sentry.BrowserTracing({
+      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+      tracePropagationTargets: [
+        'localhost',
+        'https://mpc-recovery-leader-mainnet-tmp-cg7nolnlpa-ue.a.run.app',
+        'https://api.kitwallet.app',
+        'https://mpc-recovery-leader-dev-7tk2cmmtcq-ue.a.run.app',
+        'https://testnet-api.kitwallet.app'
+      ],
+    }),
+    new Sentry.Replay(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate:         1.0, // Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 const container = document.getElementById('root');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const {
+  sentryWebpackPlugin
+} = require("@sentry/webpack-plugin");
+
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,43 +14,43 @@ module.exports = {
     path:       path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer:  ['buffer', 'Buffer']
-    }),
-    new webpack.EnvironmentPlugin({
-      DEBUG:                                true,
-      REACT_APP_BASE_PATH:                  '',
-      NETWORK_ID:                           'mainnet',
-      RELAYER_URL:                          'https://near-relayer-mainnet.api.pagoda.co/relay',
-      FIREBASE_API_KEY:                     'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
-      FIREBASE_AUTH_DOMAIN:                 'near-fastauth-prod.firebaseapp.com',
-      FIREBASE_PROJECT_ID:                  'near-fastauth-prod',
-      FIREBASE_STORAGE_BUCKET:              'near-fastauth-prod.appspot.com',
-      FIREBASE_MESSAGING_SENDER_ID:         '829449955812',
-      FIREBASE_APP_ID:                      '1:829449955812:web:532436aa35572be60abff1',
-      FIREBASE_MEASUREMENT_ID:              'G-T2PPJ8QRYY',
-      RELAYER_URL_TESTNET:                  'http://34.70.226.83:3030/relay',
-      FIREBASE_API_KEY_TESTNET:             'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
-      FIREBASE_AUTH_DOMAIN_TESTNET:         'pagoda-oboarding-dev.firebaseapp.com',
-      FIREBASE_PROJECT_ID_TESTNET:          'pagoda-oboarding-dev',
-      FIREBASE_STORAGE_BUCKET_TESTNET:      'pagoda-oboarding-dev.appspot.com',
-      FIREBASE_MESSAGING_SENDER_ID_TESTNET: '116526963563',
-      FIREBASE_APP_ID_TESTNET:              '1:116526963563:web:053cb0c425bf514007ca2e',
-      FIREBASE_MEASUREMENT_ID_TESTNET:      'G-HF2NBGE60S'
-    })
-  ],
+  plugins: [new HtmlWebpackPlugin({
+    template: path.join(__dirname, 'public', 'index.html'),
+  }), new webpack.ProvidePlugin({
+    process: 'process/browser',
+    Buffer:  ['buffer', 'Buffer']
+  }), new webpack.EnvironmentPlugin({
+    DEBUG:                                true,
+    REACT_APP_BASE_PATH:                  '',
+    NETWORK_ID:                           'mainnet',
+    RELAYER_URL:                          'https://near-relayer-mainnet.api.pagoda.co/relay',
+    FIREBASE_API_KEY:                     'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
+    FIREBASE_AUTH_DOMAIN:                 'near-fastauth-prod.firebaseapp.com',
+    FIREBASE_PROJECT_ID:                  'near-fastauth-prod',
+    FIREBASE_STORAGE_BUCKET:              'near-fastauth-prod.appspot.com',
+    FIREBASE_MESSAGING_SENDER_ID:         '829449955812',
+    FIREBASE_APP_ID:                      '1:829449955812:web:532436aa35572be60abff1',
+    FIREBASE_MEASUREMENT_ID:              'G-T2PPJ8QRYY',
+    RELAYER_URL_TESTNET:                  'http://34.70.226.83:3030/relay',
+    FIREBASE_API_KEY_TESTNET:             'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
+    FIREBASE_AUTH_DOMAIN_TESTNET:         'pagoda-oboarding-dev.firebaseapp.com',
+    FIREBASE_PROJECT_ID_TESTNET:          'pagoda-oboarding-dev',
+    FIREBASE_STORAGE_BUCKET_TESTNET:      'pagoda-oboarding-dev.appspot.com',
+    FIREBASE_MESSAGING_SENDER_ID_TESTNET: '116526963563',
+    FIREBASE_APP_ID_TESTNET:              '1:116526963563:web:053cb0c425bf514007ca2e',
+    FIREBASE_MEASUREMENT_ID_TESTNET:      'G-HF2NBGE60S'
+  }), sentryWebpackPlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: "pagoda-o6",
+    project: "javascript-react"
+  })],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
     port: 3000,
   },
-  devtool: 'eval-source-map',
+  devtool: "source-map",
   module:  {
     // exclude node_modules
     rules: [
