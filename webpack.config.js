4f20f19068d12621f3042acfc02e928597e09af9
Danny Cho <hj.danny.cho@gmail.com>
2023-11-03 12:06:40 +1300

    chore: revert to previous format

--- webpack.config.js
+++ webpack.config.js
@@ -10,33 +10,37 @@ module.exports = {
     path:       path.resolve(__dirname, 'dist'),
     publicPath: '/',
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: path.join(__dirname, 'public', 'index.html'),
-  }), new webpack.ProvidePlugin({
-    process: 'process/browser',
-    Buffer:  ['buffer', 'Buffer']
-  }), new webpack.EnvironmentPlugin({
-    DEBUG:                                true,
-    REACT_APP_BASE_PATH:                  '',
-    NETWORK_ID:                           'mainnet',
-    RELAYER_URL:                          'https://near-relayer-mainnet.api.pagoda.co/relay',
-    FIREBASE_API_KEY:                     'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
-    FIREBASE_AUTH_DOMAIN:                 'near-fastauth-prod.firebaseapp.com',
-    FIREBASE_PROJECT_ID:                  'near-fastauth-prod',
-    FIREBASE_STORAGE_BUCKET:              'near-fastauth-prod.appspot.com',
-    FIREBASE_MESSAGING_SENDER_ID:         '829449955812',
-    FIREBASE_APP_ID:                      '1:829449955812:web:532436aa35572be60abff1',
-    FIREBASE_MEASUREMENT_ID:              'G-T2PPJ8QRYY',
-    RELAYER_URL_TESTNET:                  'http://34.70.226.83:3030/relay',
-    FIREBASE_API_KEY_TESTNET:             'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
-    FIREBASE_AUTH_DOMAIN_TESTNET:         'pagoda-oboarding-dev.firebaseapp.com',
-    FIREBASE_PROJECT_ID_TESTNET:          'pagoda-oboarding-dev',
-    FIREBASE_STORAGE_BUCKET_TESTNET:      'pagoda-oboarding-dev.appspot.com',
-    FIREBASE_MESSAGING_SENDER_ID_TESTNET: '116526963563',
-    FIREBASE_APP_ID_TESTNET:              '1:116526963563:web:053cb0c425bf514007ca2e',
-    FIREBASE_MEASUREMENT_ID_TESTNET:      'G-HF2NBGE60S',
-    SENTRY_DSN:                           'https://1049553ebca8337848160ca53a49ff2a@o398573.ingest.sentry.io/4506148066164736',
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer:  ['buffer', 'Buffer']
+    }),
+    new webpack.EnvironmentPlugin({
+      DEBUG:                                true,
+      REACT_APP_BASE_PATH:                  '',
+      NETWORK_ID:                           'mainnet',
+      RELAYER_URL:                          'https://near-relayer-mainnet.api.pagoda.co/relay',
+      FIREBASE_API_KEY:                     'AIzaSyDhxTQVeoWdnbpYTocBAABbLULGf6H5khQ',
+      FIREBASE_AUTH_DOMAIN:                 'near-fastauth-prod.firebaseapp.com',
+      FIREBASE_PROJECT_ID:                  'near-fastauth-prod',
+      FIREBASE_STORAGE_BUCKET:              'near-fastauth-prod.appspot.com',
+      FIREBASE_MESSAGING_SENDER_ID:         '829449955812',
+      FIREBASE_APP_ID:                      '1:829449955812:web:532436aa35572be60abff1',
+      FIREBASE_MEASUREMENT_ID:              'G-T2PPJ8QRYY',
+      RELAYER_URL_TESTNET:                  'http://34.70.226.83:3030/relay',
+      FIREBASE_API_KEY_TESTNET:             'AIzaSyDAh6lSSkEbpRekkGYdDM5jazV6IQnIZFU',
+      FIREBASE_AUTH_DOMAIN_TESTNET:         'pagoda-oboarding-dev.firebaseapp.com',
+      FIREBASE_PROJECT_ID_TESTNET:          'pagoda-oboarding-dev',
+      FIREBASE_STORAGE_BUCKET_TESTNET:      'pagoda-oboarding-dev.appspot.com',
+      FIREBASE_MESSAGING_SENDER_ID_TESTNET: '116526963563',
+      FIREBASE_APP_ID_TESTNET:              '1:116526963563:web:053cb0c425bf514007ca2e',
+      FIREBASE_MEASUREMENT_ID_TESTNET:      'G-HF2NBGE60S',
+      SENTRY_DSN:                           'https://1049553ebca8337848160ca53a49ff2a@o398573.ingest.sentry.io/4506148066164736',
+    })
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
