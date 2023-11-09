a653875e98102641d74116dbc798b882afeca13d
Daryl Collins <daryl@darylcollins.com>
2023-11-09 11:04:23 -0800

    Initial e2e webapp test configuration

--- packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
+++ packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
@@ -10,19 +10,19 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir:       './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly:    !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries:       process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers:       process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter:      'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+  use:           {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
@@ -34,17 +34,17 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use:  { ...devices['Desktop Chrome'] },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use:  { ...devices['Desktop Firefox'] },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use:  { ...devices['Desktop Safari'] },
     },
 
     /* Test against mobile viewports. */
@@ -68,10 +68,16 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  /* Run your local dev serve`r` before starting the tests */
+  webServer: [{
+    command:             'cd ../near-fast-auth-signer && yarn run start',
+    url:                 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+  {
+    command:             'yarn run start',
+    url:                 'http://127.0.0.1:3030',
+    reuseExistingServer: !process.env.CI,
+  }
+  ],
 });
---
+++ packages/near-fast-auth-signer-e2e-tests/test-app/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
