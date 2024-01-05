bfbb5af44c8acb6fb13d2b119185ad50bc5b0ea9
Felipe Pessina <fs.pessina@gmail.com>
2024-01-05 17:40:48 -0300

    Add loading on Login screen

--- packages/near-fast-auth-signer/src/components/Login/Login.tsx
+++ packages/near-fast-auth-signer/src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { fetchSignInMethodsForEmail } from 'firebase/auth';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import * as yup from 'yup';
@@ -8,6 +8,7 @@ import * as yup from 'yup';
 import { LoginWrapper } from './Login.style';
 import { Button } from '../../lib/Button';
 import Input from '../../lib/Input/Input';
+import { Spinner } from '../../lib/Spinner';
 import { openToast } from '../../lib/Toast';
 import { useHandleAuthenticationFlow } from '../../utils/auth';
 import { getFirebaseAuth } from '../../utils/firebase';
@@ -24,25 +25,35 @@ function Login() {
   const navigate = useNavigate();
   const handleAuthenticationFlow = useHandleAuthenticationFlow();
   const skipGetKeys = currentSearchParams.get('skipGetKey') === 'true';
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    const isRecovery = currentSearchParams.get('isRecovery');
-    const email = currentSearchParams.get('email');
-    if (isRecovery) {
-      if (isRecovery === 'true' && email) {
-        handleAuthenticationFlow(currentSearchParams.get('email'), skipGetKeys);
-      } else if (isRecovery === 'true' && !email) {
-        navigate({
-          pathname: '/login',
-          search:   currentSearchParams.toString(),
-        });
-      } else {
-        navigate({
-          pathname: '/create-account',
-          search:   currentSearchParams.toString(),
-        });
+    const handleCheck = async () => {
+      const isRecovery = currentSearchParams.get('isRecovery');
+      const email = currentSearchParams.get('email');
+      if (isRecovery) {
+        if (isRecovery === 'true' && email) {
+          try {
+            setIsLoading(true);
+            await handleAuthenticationFlow(currentSearchParams.get('email'), skipGetKeys);
+          } finally {
+            setIsLoading(false);
+          }
+        } else if (isRecovery === 'true' && !email) {
+          navigate({
+            pathname: '/login',
+            search:   currentSearchParams.toString(),
+          });
+        } else {
+          navigate({
+            pathname: '/create-account',
+            search:   currentSearchParams.toString(),
+          });
+        }
       }
-    }
+    };
+
+    handleCheck();
   }, [currentSearchParams, handleAuthenticationFlow, navigate, skipGetKeys]);
 
   const { handleSubmit, register, formState: { errors } } = useForm({
@@ -57,14 +68,19 @@ function Login() {
     params: { email: string }
   ) => {
     fetchSignInMethodsForEmail(getFirebaseAuth(), params.email)
-      .then((result) => {
+      .then(async (result) => {
         if (result.length === 0) {
           navigate({
             pathname: '/create-account',
             search:   `email=${params.email}`,
           });
         } else if (result[0] === 'emailLink') {
-          handleAuthenticationFlow(params.email, skipGetKeys);
+          try {
+            setIsLoading(true);
+            await handleAuthenticationFlow(params.email, skipGetKeys);
+          } finally {
+            setIsLoading(false);
+          }
         }
       })
       .catch((error: any) => {
@@ -78,27 +94,29 @@ function Login() {
 
   return (
     <LoginWrapper>
-      <form onSubmit={handleSubmit(emailCheck)}>
-        <header>
-          <h1 data-test-id="heading_login">Log In</h1>
-          <p className="desc">Please enter your email</p>
-        </header>
-        <Input
-          {...register('email')}
-          placeholder="user_name@email.com"
-          type="email"
-          dataTest={{ input: 'email_login' }}
-          required
-          error={errors.email?.message}
-        />
-        <Button
-          size="large"
-          type="submit"
-          label="Continue"
-          variant="affirmative"
-          data-test-id="login_button"
-        />
-      </form>
+      {isLoading ? <Spinner /> : (
+        <form onSubmit={handleSubmit(emailCheck)}>
+          <header>
+            <h1 data-test-id="heading_login">Log In</h1>
+            <p className="desc">Please enter your email</p>
+          </header>
+          <Input
+            {...register('email')}
+            placeholder="user_name@email.com"
+            type="email"
+            dataTest={{ input: 'email_login' }}
+            required
+            error={errors.email?.message}
+          />
+          <Button
+            size="large"
+            type="submit"
+            label="Continue"
+            variant="affirmative"
+            data-test-id="login_button"
+          />
+        </form>
+      )}
     </LoginWrapper>
   );
 }
