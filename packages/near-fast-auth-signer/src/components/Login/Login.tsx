4dd25a1cbc45c5c90eb74571af610c3a333d4123
benmalcom <benjaminikeji@gmail.com>
2024-01-14 20:23:23 +0100

    Refactor pages for iFrame use

--- packages/near-fast-auth-signer/src/components/Login/Login.tsx
+++ packages/near-fast-auth-signer/src/components/Login/Login.tsx
@@ -1,12 +1,18 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
 import * as yup from 'yup';
 
-import { LoginWrapper } from './Login.style';
+import { SeparatorWrapper, Separator } from './Login.style';
+import useElementHeightForIframe from '../../hooks/useElementHeightForIframe';
 import { Button } from '../../lib/Button';
 import Input from '../../lib/Input/Input';
+import { openToast } from '../../lib/Toast';
+import { inIframe } from '../../utils';
+import { userExists } from '../../utils/firebase';
+import { FormContainer, StyledContainer } from '../Layout';
 
 const schema = yup.object().shape({
   email: yup
@@ -15,16 +21,23 @@ const schema = yup.object().shape({
     .required('Please enter a valid email address'),
 });
 
+const LoginForm = styled(FormContainer)`
+  height: 400px;
+`;
+
 function Login() {
+  // Send form height to modal if in iframe
+  useElementHeightForIframe(document.querySelector('#loginForm'));
+
   const [currentSearchParams] = useSearchParams();
   const navigate = useNavigate();
 
-  useEffect(() => {
+  /*  useEffect(() => {
     const isRecovery = currentSearchParams.get('isRecovery');
     if (isRecovery) {
       if (isRecovery === 'true') {
         navigate({
-          pathname: '/add-device',
+          pathname: '/login',
           search:   currentSearchParams.toString(),
         });
       } else {
@@ -34,7 +47,7 @@ function Login() {
         });
       }
     }
-  }, [currentSearchParams, navigate]);
+  }, [currentSearchParams, navigate]); */
 
   const { handleSubmit, register, formState: { errors } } = useForm({
     mode:          'all',
@@ -54,15 +67,16 @@ function Login() {
   };
 
   return (
-    <LoginWrapper>
-      <form onSubmit={handleSubmit(emailCheck)}>
+    <StyledContainer inIframe={inIframe()}>
+
+      <LoginForm id="loginForm" inIframe={inIframe()} onSubmit={handleSubmit(emailCheck)}>
         <header>
           <h1 data-test-id="heading_login">Log In</h1>
           <p className="desc">Please enter your email</p>
         </header>
         <Input
           {...register('email')}
-          placeholder="user_name@email.com"
+          placeholder="your@email.com"
           type="email"
           dataTest={{ input: 'email_login' }}
           required
@@ -75,8 +89,21 @@ function Login() {
           variant="affirmative"
           data-test-id="login_button"
         />
-      </form>
-    </LoginWrapper>
+
+        <SeparatorWrapper>
+          <Separator />
+          Or
+          <Separator />
+        </SeparatorWrapper>
+        <Button
+          size="large"
+          label="Connect Wallet"
+          variant="secondary"
+          data-test-id="connect_wallet_button"
+          iconLeft="bi bi-wallet"
+        />
+      </LoginForm>
+    </StyledContainer>
   );
 }
 
