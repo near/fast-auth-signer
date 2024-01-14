ba7f3c135bfb206c868ff5bd12f5a49b5e6d7473
benmalcom <benjaminikeji@gmail.com>
2024-01-14 20:22:51 +0100

    Update login styles

--- packages/near-fast-auth-signer/src/components/Login/Login.style.ts
+++ packages/near-fast-auth-signer/src/components/Login/Login.style.ts
@@ -1,27 +1,16 @@
 import styled from 'styled-components';
 
-export const LoginWrapper = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #f2f1ea;
+export const SeparatorWrapper = styled.div`
   display: flex;
+  width: 100%;
+  justify-content: space-between;
   align-items: center;
-  justify-content: center;
-  padding: 0 16px;
-  padding-bottom: 60px;
+  color: #868682;
+  font-size: 12px;
+`;
 
-  form {
-    max-width: 360px;
-    width: 100%;
-    margin: 16px auto;
-    background-color: #ffffff;
-    padding: 25px;
-    border-radius: 12px;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-  }
-  button {
-    margin-top: 20px;
-  }
+export const Separator = styled.div`
+  width: 140px;
+  height: 1px;
+  background-color: #EEEEEC;
 `;
