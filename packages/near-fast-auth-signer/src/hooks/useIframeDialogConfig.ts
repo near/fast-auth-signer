3175e30e79bd8fa33bb83839ac9f3bbefbadc0f8
benmalcom <benjaminikeji@gmail.com>
2024-01-16 16:26:05 +0100

    Update hook to send multiple options

--- packages/near-fast-auth-signer/src/hooks/useIframeDialogConfig.ts
+++ packages/near-fast-auth-signer/src/hooks/useIframeDialogConfig.ts
@@ -1,40 +1,56 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect } from 'react';
 
 import { inIframe } from '../utils';
 
+// Define the return type for the hook
 type HookReturnType = {
-  sendDialogHeight(): void;
+  // eslint-disable-next-line no-unused-vars
+  sendDialogHeight: (domElement?: HTMLElement) => void;
 };
 
+// Define configuration options for the hook
 type ConfigOptions = {
   source?: string;
-  element: HTMLElement;
+  // Containing element in the iframe
+  element?: HTMLElement;
   onClose?: () => void;
 };
 
-const useIframeDialogConfig = ({ element, onClose, source }: ConfigOptions): HookReturnType => {
-  const [isMounted, setIsMounted] = useState(false);
+/**
+ * Custom hook to configure and handle the behavior of an iframe modal.
+ * @param {ConfigOptions} options - Configuration options for the hook.
+ * @returns {HookReturnType} An object containing the function to send the iframe height.
+ */
+const useIframeDialogConfig = (options: ConfigOptions): HookReturnType => {
+  const { element, onClose, source } = options;
+  /**
+   * Callback function to send the iframe height to the parent window.
+   * @param {HTMLElement} domElement - Optional parameter to specify the target element.
+   */
+  const sendDialogHeight = useCallback((domElement?: HTMLElement) => {
+    // Check if the component is rendered inside an iframe
+    if (!inIframe()) return;
 
-  useEffect(() => {
-    // Set component as mounted to make sure element is available in DOM
-    setIsMounted(true);
-  }, []);
+    const el = domElement ?? element;
 
-  const sendDialogHeight = useCallback(() => {
-    if (!inIframe() || !isMounted) return;
-    if (element) {
-      const dialogHeight = element.offsetHeight;
-      const data = { dialogHeight } as Record<string, unknown>;
-      if (source) data.source = source;
-      if (onClose) data.onClose = onClose.toString();
-      window.parent.postMessage(data, '*');
+    // Check if the element exists
+    if (el) {
+      // Get the height of the element
+      const dialogHeight = el.offsetHeight;
+
+      console.log('dialogHeight ', dialogHeight);
+
+      // Send the height, onClose, and source to the parent window
+      window.parent.postMessage({ dialogHeight, onClose, source }, '*');
     }
-  }, [element, isMounted, onClose, source]);
+  }, [element, onClose, source]);
 
+  // Effect to send iframe height on mount and when the sendIframeHeight function changes
   useEffect(() => {
     sendDialogHeight();
   }, [sendDialogHeight]);
 
+  // Return the function to send the dialog (iframe) height
   return { sendDialogHeight };
 };
 
