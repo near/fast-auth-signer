5b6f21e8da855e559399da8a6398a980b18956cc
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-02-12 17:50:17 +0100

    Remove unnecessary promise wrapper

--- packages/near-fast-auth-signer/src/hooks/useFirebaseUser.ts
+++ packages/near-fast-auth-signer/src/hooks/useFirebaseUser.ts
@@ -2,25 +2,19 @@ import { useState, useEffect } from 'react';
 
 import { firebaseAuth } from '../utils/firebase';
 
-// Custom hook to manage Firebase auth state
 function useFirebaseUser() {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true); // Tracks the loading state
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // This function returns a promise that resolves once Firebase auth state is known
-    const getAuthState = () => new Promise((resolve) => {
-      const unsubscribe = firebaseAuth.onAuthStateChanged((user) => {
-        resolve(user);
-        unsubscribe(); // Cleanup the subscription
-      });
+    const unsubscribe = firebaseAuth.onAuthStateChanged((data) => {
+      setUser(data);
+      setLoading(false);
     });
 
-    // Call getAuthState and update the state based on its result
-    getAuthState().then((user) => {
-      setUser(user);
-      setLoading(false); // Set loading to false once we have the auth state
-    });
+    return () => {
+      unsubscribe(); // Cleanup the subscription when component unmounts
+    };
   }, []);
 
   return { user, loading };
