1fe9971ea8d303d28d4cf826fc13195e50aefed9
Danny Cho <hj.danny.cho@gmail.com>
2024-03-05 17:25:53 +1300

    fix: update the SignMultichain component to use same getAuthState like Sign

--- packages/near-fast-auth-signer/src/components/SignMultichain/SignMultichain.tsx
+++ packages/near-fast-auth-signer/src/components/SignMultichain/SignMultichain.tsx
@@ -1,5 +1,7 @@
 import { borshDeserialize } from 'borsher';
-import * as React from 'react';
+import React, {
+  useEffect, useState, useCallback
+} from 'react';
 
 import { derivationPathSchema } from './schema';
 import { DerivationPathDeserialized, MultichainIframeMessage } from './types';
@@ -14,7 +16,9 @@ import {
 import InternetSvg from '../../Images/Internet';
 import ModalIconSvg from '../../Images/ModalIcon';
 import { Button, CloseButton } from '../../lib/Button';
-import { useAuthState } from '../../lib/useAuthState';
+import { getAuthState } from '../../hooks/useAuthState';
+import useFirebaseUser from '../../hooks/useFirebaseUser';
+import useIframeDialogConfig from '../../hooks/useIframeDialogConfig';
 import { ModalSignWrapper } from '../Sign/Sign.styles';
 import TableContent from '../TableContent/TableContent';
 import { TableRow } from '../TableRow/TableRow';
@@ -29,21 +33,20 @@ const sampleMessageForEthereum: MultichainIframeMessage = {
 };
 
 function SignMultichain() {
-  const { authenticated } = useAuthState();
-  const [amountInfo, setAmountInfo] = React.useState<{ price: string | number, tokenAmount: string | number }>({ price: '...', tokenAmount: 0 });
+  const { loading: firebaseUserLoading, user: firebaseUser } = useFirebaseUser();
+  const [amountInfo, setAmountInfo] = useState<{ price: string | number, tokenAmount: string | number }>({ price: '...', tokenAmount: 0 });
+  const [message, setMessage] = useState<MultichainIframeMessage>(null);
+  const [inFlight, setInFlight] = useState(false);
+  const [error, setError] = useState(null);
+  const [deserializedDerivationPath, setDeserializedDerivationPath] = useState<DerivationPathDeserialized>(null);
 
-  const onError = (error: string) => {
-    window.parent.postMessage({ signedDelegates: '', error }, '*');
+  const onError = (text: string) => {
+    window.parent.postMessage({ signedDelegates: '', text }, '*');
+    setError(text);
   };
 
-  const [message, setMessage] = React.useState<MultichainIframeMessage>(null);
-  const [signedDelegates, setSignedDelegates] = React.useState<string>('');
-  const [deserializedDerivationPath, setDeserializedDerivationPath] = React.useState<DerivationPathDeserialized>(null);
-
-  React.useEffect(() => {
-    if (!authenticated) {
-      window.parent.postMessage({ signedDelegates: '', error: 'User not authenticated' }, '*');
-    }
+  // event listener setup
+  useEffect(() => {
     const handleMessage = (event) => {
       console.log('event', event);
       // Maybe add origin check here? if url of origin is available, maybe update below
@@ -53,88 +56,100 @@ function SignMultichain() {
       }
     };
 
-    if (authenticated === true) {
-      window.addEventListener(
-        'message',
-        handleMessage,
-      );
-
-      // TODO: test code, delete later
-      console.log('set temp message');
-      setMessage(sampleMessageForEthereum);
-    }
+    window.addEventListener(
+      'multichain-message',
+      handleMessage,
+    );
+    // TODO: test code, delete later
+    console.log('set temp message');
+    setMessage(sampleMessageForEthereum);
 
     return () => {
-      window.removeEventListener('message', handleMessage);
+      window.removeEventListener('multichain-message', handleMessage);
     };
-  }, [authenticated]);
+  }, []);
 
-  React.useEffect(() => {
+  // when we have message and price is not set, retrieve price info
+  useEffect(() => {
     if (message === null) return;
 
     if (message !== null && !('derivationPath' in message)) {
       onError('derivationPath is missing');
+      return;
     }
 
     const init = async () => {
-      if (message !== null && 'derivationPath' in message) {
-        const deserialize: DerivationPathDeserialized = borshDeserialize(derivationPathSchema, Buffer.from(message.derivationPath, 'base64'));
-        setDeserializedDerivationPath(deserialize);
-
-        const validation = await validateMessage(message, deserialize.asset);
-        if (!validation) {
-          return onError(validation.toString());
-        }
-
-        const controller = window.fastAuthController;
-        const publicKey = await controller.getPublicKey();
-        const { tokenAmount, tokenPrice } = await getTokenAndTotalPrice(deserialize.asset, message.value);
-        const transactionCost = await getGasFee({
-          asset:          deserialize.asset,
-          message,
-          usdCostOfToken: tokenPrice,
-        });
-        setAmountInfo({
-          price: transactionCost,
-          tokenAmount,
-        });
-
-        try {
-          const signedDelegateBase64 = await getSignedDelegateBase64({
-            najPublicKeyStr:            publicKey,
-            message,
-            deserializedDerivationPath: deserialize,
-          });
-          setSignedDelegates(signedDelegateBase64);
-
-          // If domain info passed from derivation path is same as window.parent.orgin, post message to parent directly
-          if (deserialize.domain === window.parent.origin) {
-            return window.parent.postMessage({ signedDelegates: signedDelegateBase64 }, '*');
-          }
-        } catch (e) {
-          throw new Error('Failed to sign delegate');
-        }
+      const deserialize: DerivationPathDeserialized = borshDeserialize(derivationPathSchema, Buffer.from(message.derivationPath, 'base64'));
+      const validation = await validateMessage(message, deserialize.asset);
+      if (!validation) {
+        return onError(validation.toString());
       }
-      return null;
+      setDeserializedDerivationPath(deserialize);
+      const { tokenAmount, tokenPrice } = await getTokenAndTotalPrice(deserialize.asset, message.value);
+      const transactionCost = await getGasFee({
+        asset:          deserialize.asset,
+        message,
+        usdCostOfToken: tokenPrice,
+      });
+      setAmountInfo({
+        price: transactionCost,
+        tokenAmount,
+      });
+      return setInFlight(false);
     };
+
     try {
-      if (authenticated) {
+      if (!amountInfo.tokenAmount && message) {
+        setInFlight(true);
         init();
       }
     } catch (e) {
       console.error('Error in init', e);
     }
-  }, [message, authenticated]);
+  }, [message, amountInfo.tokenAmount]);
+
+  const signDelegate = useCallback(async () => {
+    setError(null);
+    setInFlight(true);
+    try {
+      const controller = window.fastAuthController;
+      const publicKey = await controller.getPublicKey();
+      const signedDelegateBase64 = await getSignedDelegateBase64({
+        najPublicKeyStr:            publicKey,
+        message,
+        deserializedDerivationPath,
+      });
+      console.log('signedDelegateBase64', signedDelegateBase64);
+      window.parent.postMessage({ signedDelegates: signedDelegateBase64 }, '*');
+      setInFlight(false);
+    } catch (e) {
+      setInFlight(false);
+      throw new Error('Failed to sign delegate');
+    }
+  }, [deserializedDerivationPath, message]);
+
+  // If domain info passed from derivation path is same as window.parent.orgin, post message to parent directly
+  useEffect(() => {
+    if (deserializedDerivationPath?.domain === window.parent.origin) {
+      signDelegate();
+    }
+  }, [deserializedDerivationPath?.domain, signDelegate]);
 
   const onConfirm = async () => {
-    if (authenticated === true) {
-      console.log('signedDelegates', signedDelegates);
-      window.parent.postMessage({ signedDelegates }, '*');
+    setError(null);
+    setInFlight(true);
+    const isUserAuthenticated = await getAuthState(firebaseUser?.email);
+    if (isUserAuthenticated !== true) {
+      const errorMessage = 'You are not authenticated or there has been an indexer failure';
+      onError(errorMessage);
+      setInFlight(false);
+      return;
     }
+    await signDelegate();
   };
 
   const onCancel = () => {
-    window.parent.postMessage({ signedDelegates: '', error:  'User cancelled action' }, '*');
+    window.parent.postMessage({ signedDelegates: '', error:  'User cancelled action' });
   };
 
   return (
@@ -178,7 +193,6 @@ function SignMultichain() {
             )}
           />
         </div>
-
         <div className="table-wrapper margin-top">
           <TableContent
             leftSide="Estimated Fees"
@@ -187,16 +201,17 @@ function SignMultichain() {
           />
         </div>
       </div>
-
       <div className="modal-footer">
         <Button
           variant="affirmative"
           size="large"
-          label="Approve"
+          label={inFlight ? 'Loading...' : 'Approve'}
           onClick={onConfirm}
-          disabled={!signedDelegates}
+          disabled={!amountInfo.price || inFlight}
         />
       </div>
+      {!firebaseUserLoading && !firebaseUser && <p className="info-text">You are not authenticated!</p>}
+      {error && <p className="info-text error">{error}</p>}
     </ModalSignWrapper>
   );
 }
