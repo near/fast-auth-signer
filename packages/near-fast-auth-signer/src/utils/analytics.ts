30653b844d517ae06e0f291bb99122045d24079f
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-03-13 01:25:43 +0100

    Initialize rudderstack analytics

---
+++ packages/near-fast-auth-signer/src/utils/analytics.ts
@@ -0,0 +1,65 @@
+import { nanoid } from 'nanoid';
+import * as rudderAnalytics from 'rudder-sdk-js';
+
+import { networkId } from './config';
+
+const ANALYTICS_KEYS = {
+  testnet: '2R7K9phhzpFzk2zFIq2EFBtJ8BM',
+  mainnet: '2RIih8mrVPUTQ9uWe6TFfwXzcMe'
+};
+
+const rudderAnalyticsKey = ANALYTICS_KEYS[networkId === 'testnet' ? 'testnet' : 'mainnet'];
+const DATA_PLANE_URL = 'https://near.dataplane.rudderstack.com';
+
+let userAgentDetail: string;
+
+declare global {
+  interface Window {
+    rudderAnalytics: any;
+  }
+  interface Navigator {
+    userAgentData?: {
+      getHighEntropyValues(keys: string[]): Promise<{ [key: string]: string }>;
+    }
+  }
+}
+
+async function getUserAgentDetails() {
+  try {
+    if (navigator.userAgentData) {
+      const ua = await navigator.userAgentData.getHighEntropyValues(['platformVersion', 'model']);
+      const { model, platformVersion } = ua;
+      userAgentDetail = `${navigator.userAgent}; Platform Version: ${platformVersion}; Model: ${model}`;
+    } else {
+      userAgentDetail = navigator.userAgent;
+    }
+  } catch (error) {
+    console.error('Error getting user agent details:', error);
+    userAgentDetail = navigator.userAgent;
+  }
+}
+
+export async function initAnalytics() {
+  if (window.rudderAnalytics) return;
+  await getUserAgentDetails();
+  try {
+    rudderAnalytics.load(rudderAnalyticsKey, DATA_PLANE_URL);
+    rudderAnalytics.setAnonymousId(nanoid());
+    window.rudderAnalytics = rudderAnalytics;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export function recordEvent(eventLabel: string, properties?: Record<string, string>) {
+  if (!window.rudderAnalytics) return;
+  try {
+    rudderAnalytics.track(eventLabel, {
+      ...properties,
+      url: decodeURIComponent(window.location.href),
+      userAgentDetail
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
