edcb2940c97d7a0023ff5f707109fa57a98b3f3d
Felipe Pessina <fs.pessina@gmail.com>
2024-05-06 18:14:28 +0700

    Remove relayer URL replacer

--- packages/near-fast-auth-signer-e2e-tests/tests/login.spec.ts
+++ packages/near-fast-auth-signer-e2e-tests/tests/login.spec.ts
@@ -1,20 +1,11 @@
 import { test } from '@playwright/test';
 
-test.beforeEach(async ({ page }) => {
-  await page.route('https://near-relayer-testnet.api.pagoda.co/relay', async (route) => {
-    const url = new URL(route.request().url());
-    url.hostname = 'localhost';
-    url.port = '3002';
-    url.protocol = 'http';
-
-    await route.continue({ url: url.toString() });
-  });
-});
-
 test('should login', async ({ page }) => {
-  await page.goto('http://localhost:3030/');
+  await page.goto('http://localhost:3002/');
 
   await page.getByRole('button', { name: 'Sign In' }).click();
   const fastAuthIframe = page.frameLocator('#nfw-connect-iframe');
   await fastAuthIframe.getByRole('button', { name: 'Continue' }).click();
+
+  await page.waitForTimeout(20000);
 });
