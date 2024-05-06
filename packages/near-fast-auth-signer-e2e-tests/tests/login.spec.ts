ecbeaffc25151cfd9dd4c995fc1228f514023c5f
Felipe Pessina <fs.pessina@gmail.com>
2024-05-06 21:04:58 +0700

    CDP working

--- packages/near-fast-auth-signer-e2e-tests/tests/login.spec.ts
+++ packages/near-fast-auth-signer-e2e-tests/tests/login.spec.ts
@@ -1,4 +1,7 @@
-import { expect, test } from '@playwright/test';
+import crypto from 'crypto';
+
+import { faker } from '@faker-js/faker';
+import { expect, test, CDPSession } from '@playwright/test';
 
 test('should login', async ({ page }) => {
   test.slow();
@@ -30,6 +33,53 @@ test('should create account', async ({ page }) => {
   await page.waitForResponse('https://rpc.testnet.near.org/');
   await fastAuthIframe.getByRole('textbox', { name: 'user_name', exact: true }).fill('mytextplaceholder');
 
+  await fastAuthIframe.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByText('User is logged in')).toBeVisible({ timeout: 800000 });
+});
+
+async function setupVirtualAuthenticator(page) {
+  const client = await page.context().newCDPSession(page);
+  // Disable UI for automated testing
+  await client.send('WebAuthn.enable', { enableUI: false });
+
+  const result = await client.send('WebAuthn.addVirtualAuthenticator', {
+    options: {
+      protocol:                    'ctap2',
+      transport:                   'internal',
+      hasResidentKey:              true,
+      hasUserVerification:         true,
+      isUserVerified:              true,
+      automaticPresenceSimulation: true,
+    },
+  });
+  const { authenticatorId } = result;
+
+  client.on('WebAuthn.credentialAdded', () => {
+    console.log('WebAuthn.credentialAdded');
+  });
+  client.on('WebAuthn.credentialAsserted', () => {
+    console.log('WebAuthn.credentialAsserted');
+  });
+
+  return { client, authenticatorId };
+}
+
+test('test with CDP', async ({ page }) => {
+  test.slow();
+
+  await setupVirtualAuthenticator(page);
+
+  await page.goto('http://localhost:3002/');
+
+  await expect(page.getByText('User is logged in')).not.toBeVisible();
+
+  await page.getByRole('button', { name: 'Sign In' }).click();
+  const fastAuthIframe = page.frameLocator('#nfw-connect-iframe');
+
+  await fastAuthIframe.getByRole('textbox', { name: 'Email' }).click();
+  await expect(fastAuthIframe.getByText('Failed to authenticate, please retry with emai')).toBeVisible();
+  await fastAuthIframe.getByRole('textbox', { name: 'Email' }).fill('felipe@near.org');
   await fastAuthIframe.getByRole('button', { name: 'Continue' }).click();
 
   await expect(page.getByText('User is logged in')).toBeVisible({ timeout: 800000 });
