9498d610752415cf5444f5e2858032ddc5272a6e
Felipe Pessina <fs.pessina@gmail.com>
2024-05-06 13:39:11 +0700

    Setup basic fast-auth-wallet

--- packages/near-fast-auth-signer-e2e-tests/test-app/App.tsx
+++ packages/near-fast-auth-signer-e2e-tests/test-app/App.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useWalletSelector from './hooks/useWalletSelector';
 
 export default function App() {
   const selectorInstance = useWalletSelector();
+  const [fastAuthWallet, setFastAuthWallet] = useState<any>();
 
-  console.log('selectorInstance', selectorInstance);
-  if (!selectorInstance) {
+  useEffect(() => {
+    const getWallet = async () => {
+      const wallet = await selectorInstance.wallet('fast-auth-wallet');
+      setFastAuthWallet(wallet);
+    };
+
+    getWallet();
+  }, [selectorInstance]);
+
+  if (!selectorInstance || !fastAuthWallet) {
     return (
       <div id="loading-ws">Loading wallet selector...</div>
     );
   }
 
+  const handleSignIn = () => {
+    fastAuthWallet.signIn({
+      contractId: 'example.near',
+      email:      'user@example.com',
+      isRecovery: true
+    });
+  };
+
+  const handleSignUp = () => {
+    fastAuthWallet.signIn({
+      contractId: 'example.near',
+      email:      'user@example.com',
+      isRecovery: false
+    });
+  };
+
   return (
-    <div id="ws-loaded">Wallet selector instance is ready</div>
+    <div id="ws-loaded">
+      <p>Wallet selector instance is ready</p>
+      <button type="button" onClick={handleSignUp}>
+        Create Account
+      </button>
+      <button type="button" onClick={handleSignIn}>
+        Sign In
+      </button>
+    </div>
   );
 }
