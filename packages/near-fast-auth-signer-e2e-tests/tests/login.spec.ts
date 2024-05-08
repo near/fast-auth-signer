29fbacda38fd78540a7c040416cd36de273a0e57
Felipe Pessina <fs.pessina@gmail.com>
2024-05-08 16:59:20 +0700

    Overwrite passkeys methods from window inside the test

--- packages/near-fast-auth-signer-e2e-tests/tests/login.spec.ts
+++ packages/near-fast-auth-signer-e2e-tests/tests/login.spec.ts
@@ -73,7 +73,7 @@ test('should create account and login with e-mail', async ({ page, baseURL }) =>
 // The problem of this test it's that it may end up on remove device screen and be non deterministic. So I included this test on the create account.
 test('should login with email', async ({ page, baseURL }) => {
   test.slow();
-  const email = 'dded070de3-903595+xfsqk5ydbycljjl@inbox.mailtrap.io';
+  const { email, accountId } = getRandomEmailAndAccountId();
 
   await page.goto(baseURL);
 
@@ -150,6 +150,52 @@ test('should login with email', async ({ page, baseURL }) => {
   }
 
   await expect(page.getByText('User is logged in')).toBeVisible({ timeout: 800000 });
+});
+
+test('random', async ({ page, baseURL }) => {
+  const readUIDLs = [];
+
+  test.slow();
+
+  const { email, accountId } = getRandomEmailAndAccountId();
+
+  await page.goto(baseURL);
+
+  await expect(page.getByText('User is logged in')).not.toBeVisible();
+
+  await page.getByRole('button', { name: 'Create Account' }).click();
+  const fastAuthIframe = page.frameLocator('#nfw-connect-iframe');
+
+  await fastAuthIframe.getByRole('textbox', { name: 'Email' }).fill(email);
+  await page.waitForResponse('https://rpc.testnet.near.org/');
+  await fastAuthIframe.getByRole('textbox', { name: 'user_name', exact: true }).fill(accountId);
+
+  await fastAuthIframe.getByRole('button', { name: 'Continue' }).click();
+
+  const createAccountData = await getFirebaseAuthLink(email, readUIDLs, {
+    user:     process.env.MAILTRAP_USER,
+    password: process.env.MAILTRAP_PASS,
+    host:     'pop3.mailtrap.io',
+    port:     9950,
+    tls:      false
+  });
+
+  readUIDLs.push(createAccountData.uidl);
+
+  expect(createAccountData.link).toBeTruthy();
+
+  await page.addInitScript(() => {
+    console.log('called here felipe');
+    window.PublicKeyCredential.isUserVerifyingPlatformAuthenticatorAvailable = async () => true;
+    navigator.credentials.create = async (...args) => {
+      console.log('navigator.credentials.create was called');
+      return args as any;
+    };
+  });
+
+  await page.goto(createAccountData.link);
+
+  await expect(page.getByText('User is logged in')).toBeVisible({ timeout: 900000 });
 });
 
 test('should login with passkey', async ({ page }) => {
