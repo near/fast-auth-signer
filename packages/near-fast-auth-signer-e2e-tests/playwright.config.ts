5f00b2263fede93ff00b3f5d35b644c59c91eca5
Felipe Pessina <fs.pessina@gmail.com>
2024-05-14 19:37:14 +0700

    Wait for relayer to start

--- packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
+++ packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
@@ -39,15 +39,15 @@ export default defineConfig<TestOptions>({
       name: 'chromium',
       use:  { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use:  { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use:  { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use:  { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use:  { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
