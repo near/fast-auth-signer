c0a0cdc0f95f0a6a9a77f08d5efe05335fe852d4
Felipe Pessina <fs.pessina@gmail.com>
2024-05-15 22:46:34 +0700

    Update timeout

--- packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
+++ packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
@@ -13,14 +13,11 @@ require('dotenv').config();
  */
 export default defineConfig<TestOptions>({
   testDir:       './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly:    !!process.env.CI,
-  /* Retry on CI only */
-  retries:       process.env.CI ? 2 : 2,
-  /* Opt out of parallel tests on CI. */
-  workers:       3,
+  retries:       2,
+  workers:       process.env.CI ? 3 : 6,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:      'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -29,7 +26,7 @@ export default defineConfig<TestOptions>({
     baseURL:    'http://127.0.0.1:3001/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
