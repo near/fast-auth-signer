74e0fe4803649e98150975b49d7298f0a6bbb112
Felipe Pessina <fs.pessina@gmail.com>
2024-05-15 22:35:39 +0700

    Update worker amount

--- packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
+++ packages/near-fast-auth-signer-e2e-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig<TestOptions>({
   /* Retry on CI only */
   retries:       process.env.CI ? 2 : 2,
   /* Opt out of parallel tests on CI. */
-  workers:       6,
+  workers:       3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter:      'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
