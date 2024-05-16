51f54f9b520b33cc4ad1c404f75ff6a4c5f22511
Danny Cho <hj.danny.cho@gmail.com>
2024-05-17 10:58:39 +1200

    chore: remove files, created serviceAccount

--- packages/near-fast-auth-signer-e2e-tests/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export const generateRandomString = (length) => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i += 1) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-};
