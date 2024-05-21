a296b5790ddaeeaa19736d3db66b31071144dda6
Danny Cho <hj.danny.cho@gmail.com>
2024-05-21 13:56:20 +1200

    fix: change to single environment variable

--- packages/near-fast-auth-signer-e2e-tests/utils/serviceAccount.js
+++ packages/near-fast-auth-signer-e2e-tests/utils/serviceAccount.js
@@ -1,13 +1,1 @@
-export const serviceAccount = {
-  type:                        'service_account',
-  project_id:                  'pagoda-oboarding-dev',
-  private_key_id:              process.env.FIREBASE_PRIVATE_KEY_ID_TESTNET,
-  private_key:                 Buffer.from(process.env.FIREBASE_PRIVATE_KEY_TETNET, 'base64').toString('utf-8') || '',
-  client_email:                process.env.FIRESBASE_CLIENT_EMAIL_TESTNET,
-  client_id:                   process.env.FIRESBASE_CLIENT_ID_TESTNET,
-  auth_uri:                    'https://accounts.google.com/o/oauth2/auth',
-  token_uri:                   'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url:        process.env.FIRESBASE_CLIENT_CERT_TESTNET,
-  universe_domain:             'googleapis.com'
-};
+export const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_TESTNET || '{}');
