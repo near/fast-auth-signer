23152b55f3668f45b1dffe8041acf45f7b54d40a
Danny Cho <hj.danny.cho@gmail.com>
2024-05-31 17:36:18 +1200

    fix: increased timeout to 60000

--- packages/near-fast-auth-signer-e2e-tests/utils/constants.ts
+++ packages/near-fast-auth-signer-e2e-tests/utils/constants.ts
@@ -2,4 +2,4 @@ import { Page } from 'playwright/test';
 
 export const getFastAuthIframe = (page: Page) => page.frameLocator('#nfw-connect-iframe');
 
-export const TIMEOUT = 30000;
+export const TIMEOUT = 60000;
