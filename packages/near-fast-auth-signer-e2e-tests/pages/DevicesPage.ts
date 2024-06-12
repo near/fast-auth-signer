f9e978d9b785cfa2fea97a6cf4223d9320a06ecd
Danny Cho <hj.danny.cho@gmail.com>
2024-06-12 14:58:00 +1200

    fix: remove wait

--- packages/near-fast-auth-signer-e2e-tests/pages/DevicesPage.ts
+++ packages/near-fast-auth-signer-e2e-tests/pages/DevicesPage.ts
@@ -16,8 +16,6 @@ class DevicesPage {
 
   async selectAndDelete(numberOfCheckboxes: number) {
     await this.page.waitForSelector('[data-testid^="devices-checkbox-"]');
-    // wait for the checkboxes to ready
-    await this.page.waitForTimeout(3000);
     const deleteButtonTarget = this.page.getByTestId('devices-delete-key');
     const checkboxes = await this.page.locator('[data-testid^="devices-checkbox-"]');
 
@@ -27,12 +25,7 @@ class DevicesPage {
       // check to make sure if the checkbox is ready to be clicked
       const checkboxSelector = `[data-testid="devices-checkbox-${i}"]`;
       await this.page.waitForSelector(checkboxSelector, { state: 'visible' });
-
-      await checkboxes.nth(i).scrollIntoViewIfNeeded();
       await checkboxes.nth(i).click();
-
-      // wait for click action to finish smoothly
-      await this.page.waitForTimeout(1000);
     }
 
     await deleteButtonTarget.click({ force: true });
