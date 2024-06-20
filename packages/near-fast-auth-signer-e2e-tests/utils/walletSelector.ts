80838faddeba91b370265ff47350aa9ce62ce41d
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-06-20 06:12:18 +0100

    Remove unnecessary await

--- packages/near-fast-auth-signer-e2e-tests/utils/walletSelector.ts
+++ packages/near-fast-auth-signer-e2e-tests/utils/walletSelector.ts
@@ -1,6 +1,6 @@
 import { expect, Page } from 'playwright/test';
 
 export const isWalletSelectorLoaded = async (page: Page) => {
-  const walletSelector = await page.getByTestId('app-container');
+  const walletSelector = page.getByTestId('app-container');
   await expect(walletSelector).toBeVisible();
 };
