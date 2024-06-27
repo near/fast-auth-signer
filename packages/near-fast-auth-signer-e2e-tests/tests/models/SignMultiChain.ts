7786bf2393f85a93d61ead85dd8ad343048cb480
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-06-27 19:14:26 +0100

    Fix webkit issues

--- packages/near-fast-auth-signer-e2e-tests/tests/models/SignMultiChain.ts
+++ packages/near-fast-auth-signer-e2e-tests/tests/models/SignMultiChain.ts
@@ -50,14 +50,13 @@ class SignMultiChain {
     await this.page.check(`input#${assetType.toLowerCase()}`);
     await this.page.fill('input#amount', `${amount}`);
     await this.page.fill('input#address', `${address}`);
-    await this.page.click('button[type="submit"]');
 
     // Webkit browsers only trigger submit if button is double-clicked, this is only happening with our test dapp
-    /* if (this.isWebkit && !process.env.CI) {
+    if (this.isWebkit && !process.env.CI) {
       await this.page.dblclick('button[type="submit"]');
     } else {
       await this.page.click('button[type="submit"]');
-    } */
+    }
   }
 
   async clickApproveButton() {
