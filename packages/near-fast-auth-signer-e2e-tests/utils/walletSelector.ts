5d9863431af3c95e5dfbcb431d048cd76b56a021
Ikeji Benjamin <benjaminikeji@gmail.com>
2024-06-27 17:03:27 +0100

    Add util functions

--- packages/near-fast-auth-signer-e2e-tests/utils/walletSelector.ts
+++ packages/near-fast-auth-signer-e2e-tests/utils/walletSelector.ts
@@ -4,3 +4,15 @@ export const isWalletSelectorLoaded = async (page: Page) => {
   const walletSelector = page.getByTestId('app-container');
   await expect(walletSelector).toBeVisible();
 };
+
+export const ensureIframeIsVisible = async (page: Page) => {
+  await page.waitForSelector('#nfw-connect-iframe');
+  await expect(page.locator('#nfw-connect-iframe')).toBeVisible();
+};
+
+export const getIframeElement = async (page: Page) => {
+  const iframeElementHandle = await page.$('#nfw-connect-iframe');
+  const iframe = await iframeElementHandle.contentFrame();
+  await iframe.waitForLoadState('domcontentloaded');
+  return iframe;
+};
